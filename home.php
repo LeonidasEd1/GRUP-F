c0d8d9619dd0a31c38108df9172c33522e03fdfd
condoriveronica <92111235+condoriveronica@users.noreply.github.com>
2021-12-13 09:47:54 -0500

    Update home.php

--- home.php
+++ home.php
@@ -17,4 +17,22 @@
     </div>
  </div>
 </div>
+<div class="modal" id="img1">
+		<h3>imagenes electrodomesticos</h3>
+		<div class="imagen">
+			<a href="#img4">&#60;</a>
+			<a href="#img2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ90sJE9A6C54kYU09A_Jxc1bEUN3uNfkHsqmA5rxutE8z91-Ol4eZHjrkonpEQBhi-9ls&usqp=CAU"></a>
+      <a href="#img2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrIoavwUYXGXRUY1dLEQ8j5DqKn_M3cEPBWg&usqp=CAU"></a>
+			<a href="#img2">></a>
+		</div>
+		<a class="cerrar" href="">X</a>
+	</div>
+<div class="imagen">
+			<a href="#img4">&#60;</a>
+			<a href="#img2"><img src="https://i.pinimg.com/236x/19/a5/05/19a505f4a914d7b6670f6b32aa824495--image-clipart-binder.jpg"></a>
+      <a href="#img2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReGHYUY9xLwxlBzkgUa-O-4WGt7AIlLsj-1bkq6vMH3jnIlR628ZjbXrTSwAYhRgHn3K4&usqp=CAU"></a>
+			<a href="#img2">></a>
+		</div>
+		<a class="cerrar" href="">X</a>
+	</div>
 <?php include_once('layouts/footer.php'); ?>
