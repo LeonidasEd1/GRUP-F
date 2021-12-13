e722cfa4aaca05ed40700662a1b61f5f5d31e0b6
condoriveronica <92111235+condoriveronica@users.noreply.github.com>
2021-12-13 00:33:38 -0500

    Update info.php

--- nosotros/info.php
+++ nosotros/info.php
@@ -18,9 +18,59 @@ elaboración e implantación de una Política de Seguridad con el fin de:
      <marquee>
 <table align="center" width="100"border="1">
 <tr>
-   <td>Esta tabla se desplaza , con todo el contenido de la tabla</td>
+   <td>SISTEMA DE REGISTRO DE ARTICULOS ELECTRODOMESTICOS</td>
 </tr>
 </table>
 </marquee>
+    <h1>Galería de imágenes con efecto lightbox solo con CSS</h1>
+	
+	<ul class="galeria">
+		<li><a href="#img1"><img src="http://www.paisajesimagenes.com/wp-content/uploads/Descripci%C3%B3n-de-paisajes..jpg"></a></li>
+		<li><a href="#img2"><img src="http://neo-deco.es/wp-content/uploads/2013/01/Paisaje-de-monta%C3%B1a.jpg"></a></li>
+		<li><a href="#img3"><img src="http://www.paisajesimagenes.com/wp-content/uploads/Descripci%C3%B3n-de-paisajes..jpg"></a></li>
+		<li><a href="#img4"><img src="http://neo-deco.es/wp-content/uploads/2013/01/Paisaje-de-monta%C3%B1a.jpg"></a></li>
+	</ul>
+
+	<div class="modal" id="img1">
+		<h3>Paisaje 1</h3>
+		<div class="imagen">
+			<a href="#img4">&#60;</a>
+			<a href="#img2"><img src="http://www.paisajesimagenes.com/wp-content/uploads/Descripci%C3%B3n-de-paisajes..jpg"></a>
+			<a href="#img2">></a>
+		</div>
+		<a class="cerrar" href="">X</a>
+	</div>
+	
+	<div class="modal" id="img2">
+		<h3>Paisaje 2</h3>
+		<div class="imagen">
+			<a href="#img1">&#60;</a>
+			<a href="#img3"><img src="http://neo-deco.es/wp-content/uploads/2013/01/Paisaje-de-monta%C3%B1a.jpg"></a>
+			<a href="#img3">></a>
+		</div>
+		<a class="cerrar" href="">X</a>
+	</div>
+	
+	<div class="modal" id="img3">
+		<h3>Paisaje 3</h3>
+		<div class="imagen">
+			<a href="#img2">&#60;</a>
+			<a href="#img4"><img src="http://www.paisajesimagenes.com/wp-content/uploads/Descripci%C3%B3n-de-paisajes..jpg"></a>
+			<a href="#img4">></a>
+		</div>
+		<a class="cerrar" href="">X</a>
+	</div>
+	
+	<div class="modal" id="img4">
+		<h3>Paisaje 4</h3>
+		<div class="imagen">
+			<a href="#img3">&#60;</a>
+			<a href="#img1"><img src="http://neo-deco.es/wp-content/uploads/2013/01/Paisaje-de-monta%C3%B1a.jpg"></a>
+			<a href="#img1">></a>
+		</div>
+		<a class="cerrar" href="">X</a>
+	</div>
+	
+
 </body>
 </html>
