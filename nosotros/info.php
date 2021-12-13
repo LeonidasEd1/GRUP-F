fe78266bb9194d83681a010c981acd053e73c174
condoriveronica <92111235+condoriveronica@users.noreply.github.com>
2021-12-13 00:06:11 -0500

    Update info.php

---
+++ nosotros/info.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Politica y Seguridad</title>
+</head>
+<body>
+    <h1>Informacion</h1>
+    <p>Nuestro proyecto de sistema de registro de articulos electrodomesticos,
+     tenemos como objetivo hacer un sistema de inventario de productos.</p>
+     <h2> Politica y seguridad </h2>
+       <p>Establece como objetivo prioritario en materia de seguridad la
+elaboración e implantación de una Política de Seguridad con el fin de:
+
+</p>
+     
+</body>
+</html>
